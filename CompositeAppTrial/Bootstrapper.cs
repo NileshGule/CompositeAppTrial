3105c9f39ceaf2b3492df02c7694c532ed3e49a7
NileshGule <vn_nilesh@yahoo.com>
2014-02-05 01:06:31 +0800

    initial commit

---
+++ CompositeAppTrial/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace CompositeAppTrial
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
